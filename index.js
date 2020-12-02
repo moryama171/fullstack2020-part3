73886bb22cde0a7709be1f83e1c18d75846a6065
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-02 09:53:50 +0100

    Implement get/persons endpoint

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const express = require('express');
+const app = express();
 
+let persons = [
+    {
+        id: 1,
+        name: "Van Veen",
+        number: "000-12345"
+    },
+    {
+        id: 2,
+        name: "Ada Veen",
+        number: "000-6789"
+    },
+    {
+        id: 3,
+        name: "Maria Antonietta",
+        number: "111-11111"
+    },
+    {
+        id: 4,
+        name: "Luigi Augusto",
+        number: "333-33333"
+    }
+];
+
+app.get('/api/persons', (request, response) => {
+    response.send(persons);
+});
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Running on localhost:${PORT}`)
