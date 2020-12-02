444474197f1a01fdea5b4c8b1547a652e5800fe9
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-02 11:55:17 +0100

    Configure morgan for logging

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
 app.use(express.json());
 
