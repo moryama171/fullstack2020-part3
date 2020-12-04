a8582a5ed783c08782f3505246051a276edc1250
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-04 16:10:56 +0100

    Add build

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.static('build/'));
+
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
