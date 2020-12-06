59b2ba6507eef5e4803e86e78c776f73e8d146ab
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-06 11:54:25 +0100

    Set person schema to prittier view

--- models/person.js
+++ models/person.js
@@ -17,6 +17,12 @@ const personSchema = mongoose.Schema({
     number: String,
 });
 
-// TODO: Define 'toJson' method
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
 
 module.exports = mongoose.model('Person', personSchema);
