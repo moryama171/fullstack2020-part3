bc9e593ffd2c645acd0b5498120574e4afbe2457
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-06 11:52:28 +0100

    Add logging for db connection

--- models/person.js
+++ models/person.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb+srv://moryama:fullstack@cluster0.jmwof.mongodb.net/phonebook-app?retryWrites=true&w=majority';
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+console.log('connecting to', url);
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(result => {
+        console.log('connected to MongdoDB');
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB', error.message);
+    });
 
 const personSchema = mongoose.Schema({
     name: String,
