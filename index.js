1c564e26dc51251303ded00e50966138a48ceb0c
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-09 12:06:59 +0100

    Validate for unique name at update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/info', (request, response) => {
             const dateTime = new Date();
             response.send(`<p>${count}</p><p>${dateTime}</p>`);
         });
-    })
+});
 
 app.get('/api/persons', (request, response) => {
     Person.find({})
@@ -64,19 +64,23 @@ app.post('/api/persons', (request, response, next) => {
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const body = request.body;
 
     const person = {
         name: body.name,
         number: body.number,
-    }
+    };
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findOneAndUpdate({ id: request.params.id }, person, {
+        new: true,
+        runValidators: true,
+        context: 'query'
+    })
         .then(updatedPerson => {
-            response.json(updatedPerson)
+            response.json(updatedPerson);
         })
-        .catch(error => next(error))
-})
+        .catch(error => next(error));
+});
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -95,7 +99,7 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
     } else if (error.name === 'ValidationError') {
-        return response.status(303).json({ error: error.message })
+        return response.status(303).json({ error: error.message });
     }
 
     next(error);
