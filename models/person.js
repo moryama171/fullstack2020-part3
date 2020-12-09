d6d39261f5e97795e1e98a02b372563076e802a3
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-09 10:20:22 +0100

    Validate for minimum legth

--- models/person.js
+++ models/person.js
@@ -16,9 +16,13 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
 const personSchema = mongoose.Schema({
     name: {
         type: String,
+        minlength: 3,
         unique: true
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8
+    }
 });
 
 personSchema.plugin(uniqueValidator);
