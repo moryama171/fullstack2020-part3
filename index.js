2feae84c81cff23c64aa02f8d292052c124439bb
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-09 10:08:27 +0100

    Validate for unique person name

--- index.js
+++ index.js
@@ -94,6 +94,8 @@ const errorHandler = (error, request, response, next) => {
     console.log(error.message);
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(303).json({ error: error.message })
     }
 
     next(error);
